247d9ca215dc34ac2f26f526c727d9aaac240fe6
Andrzej <ganczarek.a@gmil.com>
2018-08-18 07:41:02 +0200

    Clean test-Database.cpp

--- test/tests-Database.cpp
+++ test/tests-Database.cpp
@@ -2,38 +2,26 @@
 #include "../Database.hpp"
 #include "../Student.hpp"
 #include "../Worker.hpp"
-
-/*SCENARIO("Is this string balanced?") {
-    GIVEN("string {") {
-        std::string s = {"{"};
-        WHEN("call function isBalanced()") {
-            THEN("Expect FALSE") {
-                REQUIRE_FALSE( isBalanced(s) );
+    
+SCENARIO("Find person by PESEL") {
+    GIVEN("A empty database") {
+        Database db;
+        REQUIRE_THROWS_WITH(db.showDB(),"Database is empty");
+        WHEN("Find person with wrong PESEL - 123 - in empty database") {
+            THEN("Expect throw - bad PESEL")
+            REQUIRE_THROWS_WITH(*(db.findPersonWithPESEL("123")),"Bad PESEL");
+        }
+        WHEN("Fill the database random data 5 students and 8 workers and finding person with PESEL - 66040170503 - not in the database") {
+            db.fillDB(5, 8);
+            THEN("Expect throw - There is no person with PESEL - 66040170503 in the database") 
+                REQUIRE_THROWS_WITH(*(db.findPersonWithPESEL("66040170503")),"There is no person with PESEL - 66040170503 in the database");
+        }
+        WHEN("Added person to database and find this person in database") {
+            std::shared_ptr<Person> student = std::make_shared<Student>("Katarzyna", "Nowak", "66040170503", 'W', "Wroclaw, Staszica 12", 99789);
+            db.addPerson(student);
+            THEN("Expect succes") {
+                REQUIRE(*(db.findPersonWithPESEL("66040170503")) == student);
             }
         }
     }
-    */
-    SCENARIO("Find person by PESEL") {
-        GIVEN("A empty database") {
-            Database db;
-            REQUIRE_THROWS_WITH(db.showDB(),"Database is empty");
-            WHEN("Find person with wrong PESEL - 123 - in empty database") {
-                THEN("Expect throw - bad PESEL")
-                REQUIRE_THROWS_WITH(*(db.findPersonWithPESEL("123")),"Bad PESEL");
-            }
-            WHEN("Fill the database random data 5 students and 8 workers and finding person with PESEL - 66040170503 - not in the database") {
-                db.fillDB(5, 8);
-                THEN("Expect throw - There is no person with PESEL - 66040170503 in the database") {
-                    REQUIRE_THROWS_WITH(*(db.findPersonWithPESEL("66040170503")),"There is no person with PESEL - 66040170503 in the database");
-                }
-            WHEN("Added person to database and find this person in database")
-            {
-                std::shared_ptr<Person> student = std::make_shared<Student>("Katarzyna", "Nowak", "66040170503", 'W', "Wroclaw, Staszica 12", 99789);
-                db.addPerson(student);
-                THEN("Expect succes") {
-                        REQUIRE(*(db.findPersonWithPESEL("66040170503")) == student);
-                    }
-                }
-            }
-        }
-    }
+}
